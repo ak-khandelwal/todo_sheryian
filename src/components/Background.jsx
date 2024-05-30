b3de9199342e7178f14853fa0a14d2c086687850
aryan <aryankhandelwal15@gmail.com>
2024-05-30 09:40:15 +0530

    added button

--- src/components/Background.jsx
+++ src/components/Background.jsx
@@ -2,9 +2,9 @@ import React from "react";
 
 const Background = () => {
   return (
-    <section id='background' className='absolute top-0 left-0 w-full h-screen bg-zinc-800 z-[-2]'>
+    <section id='background' className='absolute top-0 left-0 w-full bg-zinc-800 z-[-2]'>
       <header>
-        <h1 className="fixed top-1/2 left-1/2 text-[13vw] font-semibold -translate-x-[50%] -translate-y-[50%] tracking-tighter text-zinc-900">
+        <h1 className="fixed top-1/2 left-1/2 text-[13vw] font-semibold -translate-x-[50%] -translate-y-[50%] tracking-tighter text-orange-600">
           DOCS.
         </h1>
       </header>
--- src/components/Foreground.jsx
+++ src/components/Foreground.jsx
@@ -1,13 +1,23 @@
 import React from 'react';
 import Card from './Card';
+import '../CSS/Foreground.css';
 import '../CSS/Card.css';
 const Foreground = () => {
   return (
     <section id="foreground" className=''>
-        <header>
-            <h1 className='text-center p-4 text-orange-600 font-bold tracking-widest'>Documents</h1>
+        <header className=''>
+            <h1 className='relative text-center p-4 text-orange-600 font-bold tracking-widest'>Documents
+            <button type="button" className="text-white bg-orange-600 hover:bg-orange-200 hover:text-black font-medium md:rounded-lg md:text-sm md:px-5 md:py-2.5 px-3 py-1.5 me-2 mb-2 absolute right-0 top-15">Add</button>
+            </h1>
         </header>
-        <section id="cards" className=''>
+        <section id="cards" className='flex justify-center'>
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
         <Card />
         <Card />
         </section>
